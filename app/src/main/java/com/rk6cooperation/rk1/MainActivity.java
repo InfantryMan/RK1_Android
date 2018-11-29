6ccd89ef627487642808912c86ff608cb956e070
InfantryMan <rust-skate@mail.ru>
2018-11-30 00:01:06 +0300

    One more fix

--- app/src/main/java/com/rk6cooperation/rk1/MainActivity.java
+++ app/src/main/java/com/rk6cooperation/rk1/MainActivity.java
@@ -1,21 +1,16 @@
 package com.rk6cooperation.rk1;
 
-import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ContextThemeWrapper;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
@@ -74,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_add)
     void onLaunchAddClick() {
-        //TextView numberTextView = new TextView(this, null, R.style.Numbers);
         TextView numberTextView = (TextView) getLayoutInflater().inflate(R.layout.template_textview, null);
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
